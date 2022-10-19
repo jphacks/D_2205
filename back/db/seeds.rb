bb22d0aae89f9ce3057d04ff5646c999dd48cbc3
Nishinoyama <begoingto1414@gmail.com>
2022-10-20 01:12:35 +0900

    add: フェイカーを追加

--- back/db/seeds.rb
+++ back/db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+20.times do |_|
+  user = User.create(
+    name: Faker::Name.unique.last_name,
+    email: Faker::Internet.unique.email,
+  )
+  redo unless user.valid?
+  5.times.sum(rand(0..2)).times do |_|
+    post = Post.create(
+      user: user,
+      body: Faker::Lorem.paragraphs(number: rand(2..5)).join(" "),
+    )
+    redo unless post.valid?
+    post.image.attach(io: File.open(Rails.root.join('app/assets/images/dummy.png')), filename: 'dummy.png')
+  end
+end
