615b10c2fb2a364fafb60090ef4402c2eac63529
Nishinoyama <begoingto1414@gmail.com>
2022-10-20 11:49:41 +0900

    add: ブックマークのユーザのインデックス

--- back/db/migrate/20221019143438_create_bookmarks.rb
+++ back/db/migrate/20221019143438_create_bookmarks.rb
@@ -1,7 +1,7 @@
 class CreateBookmarks < ActiveRecord::Migration[7.0]
   def change
     create_table :bookmarks do |t|
-      t.references :user, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true, index: true
       t.references :post, null: false, foreign_key: true
 
       t.timestamps
