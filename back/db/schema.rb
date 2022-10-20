d1203137b404259fe49dc090ae9a756f620cf5b2
Nishinoyama <begoingto1414@gmail.com>
2022-10-21 00:15:26 +0900

    add: 都道府県の seed

--- back/db/schema.rb
+++ back/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_19_143438) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_20_150855) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -55,6 +55,12 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_19_143438) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "prefectures", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
